dbf3ae49cb31bad02fccd1242dbf5e51bebe3c0e
b0hdnan <boplan1985@gmail.com>
2023-06-05 17:58:47 +0300

    все включено

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+const storedItems = JSON.parse(localStorage.getItem)
+const [items, setItems] = React.useState(storedItems);
+
+// для першого завантаження проєкту
+
+// для зміни даних у сприску справ
+React.useEffect(()=>{
+	const temp = JSON.stringify(items)
+	localStorage.setItem("todos",temp)
+},[items])
+
+
+function addItem(){
+	let input = document.getElementById("input")
+	setItems([...items, input.value])
+	console.log(items)
+}
+function delItem(element){
+	const temp = [...items]
+	temp.splice(element,1)
+	setItems(temp)
+	localStorage.setItem("todos", temp)
+}
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+		<div className="todo">
+		<h1>To-Do List</h1>
+		<div className="form">
+			<input type="text" id='input'/>
+			<button onClick = {addItem}>Add</button>
+		</div>
+		<ul>
+			{
+
+				items.map((item, index) =>
+					(
+        <li><span>{item}</span>
+				<span className="delete"><i className="fa fa-trash" onClick = {()=>delItem(index)}></i></span>
+			</li>
+					)
+				)
+			}
+		</ul>
+	</div>
   );
 }
 
